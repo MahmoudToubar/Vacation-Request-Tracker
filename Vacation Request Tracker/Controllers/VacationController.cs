73d67526c5b9f02a15729c65383e6959c94b8e25
MahmoudToubar <110629991+MahmoudToubar@users.noreply.github.com>
2024-03-25 01:41:42 +0200

    first commit

--- Vacation Request Tracker/Controllers/VacationController.cs
+++ Vacation Request Tracker/Controllers/VacationController.cs
@@ -136,7 +136,7 @@ namespace Vacation_Request_Tracker.Controllers
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Details(Guid id)
         {
-            var request = await vacationRepositories.GetAsync(id); 
+            var request = await vacationRepositories.GetAsync(id);  
             if (request == null)
             {
                 return NotFound();
